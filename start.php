8180d4f79b8ecfc0c8669cf5dc6378e30d241d6b
Amir Reza Mehrbakhsh <amirreza@dakyaco.com>
2020-03-07 10:20:54 +0330

    Init playground

---
+++ start.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to require it into the script here so
+| we don't have to worry about manual loading any of our classes.
+|
+*/
+require __DIR__.'/vendor/autoload.php';
